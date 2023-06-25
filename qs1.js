ddd303502247e66af31e217991c3275e4460945d
Arnab <rnbpaul56@gmail.com>
2023-06-26 01:09:26 +0530

    first commit

---
+++ qs1.js
@@ -0,0 +1,34 @@
+// Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+// Example 2:
+// Input: nums = [0]
+// Output: [0]
+
+// Constraints:
+// a. 1 <= nums.length <= 10^4
+// b. -2^31 <= nums[i] <= 2^31 - 1
+
+
+function zerosAtEnd(arr){
+    let n=arr.length;
+    arr.sort((a,b)=>a-b);
+    let c=0;
+    for (let i = 0; i < n; i++) {
+        if(arr[i]!=0){
+            [arr[c],arr[i]]=[arr[i],arr[c]];
+            c++;
+
+        }
+    }
+    return arr;
+}
+
+let arr=[0,1,0,3,12];
+console.log(zerosAtEnd(arr));
